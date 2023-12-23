e9b2c19ada8d0076c7eb3b870f17e09b99b6edcf
yundoun <dbsehdns12@gmail.com>
2023-12-23 16:41:19 +0900

    Feat : myPage 작성 완료

--- src/views/ui/Badges.js
+++ src/views/ui/Badges.js
@@ -1,43 +1,43 @@
+import './style.css';
 
 
 const Badges = () => {
   return (
-    <div>
+    <div className="profile-container">
       <h2>내 정보</h2>
       <form>
-        <div>
-          <label for="name">이름:</label>
-          <input type="text" id="name" name="name" />
+        <div className="form-group">
+          <label htmlFor="email">이메일:</label>
+          <input type="email" id="email" name="email" placeholder="이메일" />
         </div>
-        <div>
-          <label for="gender">성별:</label>
+        <div className="form-group">
+          <label htmlFor="username">아이디:</label>
+          <input type="text" id="username" name="username" placeholder="아이디" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">비밀번호:</label>
+          <input type="password" id="password" name="password" placeholder="비밀번호" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">이름:</label>
+          <input type="text" id="name" name="name" placeholder="이름" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gender">성별:</label>
           <select id="gender" name="gender">
             <option value="male">남성</option>
             <option value="female">여성</option>
-            <option value="other">기타</option>
           </select>
         </div>
-        <div>
-          <label for="age">나이:</label>
-          <input type="number" id="age" name="age" />
+        <div className="form-group">
+          <label htmlFor="birthdate">생년월일:</label>
+          <input type="date" id="birthdate" name="birthdate" />
         </div>
-        <div>
-          <label for="email">이메일:</label>
-          <input type="email" id="email" name="email" />
-        </div>
-        <div>
-          <label for="username">아이디:</label>
-          <input type="text" id="username" name="username" />
-        </div>
-        <div>
-          <label for="password">비밀번호:</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <div>
-          <input type="submit" value="정보 업데이트" />
+        <div className="form-group">
+          <button className="btn">확인</button>
+          <button type="button" className="btn edit">수정</button>
         </div>
       </form>
-
     </div>
   );
 };
