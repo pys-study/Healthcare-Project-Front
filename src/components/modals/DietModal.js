a063230b0d1063f5882bab9b57f56755bb597fba
yundoun <dbsehdns12@gmail.com>
2024-01-19 21:15:14 +0900

    Style : thead 상단 고정

--- src/components/modals/DietModal.js
+++ src/components/modals/DietModal.js
@@ -20,28 +20,30 @@ const DietModal = ({ isModalOpen, closeModal, addFoodToMeal }) => {
       <div className="modal">
         <div className="modal-content">
           <span className="close" onClick={closeModal}>&times;</span>
-          <table className="food-table">
-            <thead>
-              <tr>
-                <th>이름</th>
-                <th>칼로리</th>
-                <th>탄수화물</th>
-                <th>단백질</th>
-                <th>지방</th>
-              </tr>
-            </thead>
-            <tbody>
-              {foodItems.map((item) => (
-                <tr key={item.id} onDoubleClick={() => addFoodToMeal(item)}>
-                  <td>{item.dietName}</td>
-                  <td>{item.calories}</td>
-                  <td>{item.carbs}g</td>
-                  <td>{item.protein}g</td>
-                  <td>{item.fats}g</td>
+          <div className='food-list-container'>
+            <table className="food-table">
+              <thead>
+                <tr>
+                  <th>이름</th>
+                  <th>칼로리(kcal)</th>
+                  <th>탄수화물(g)</th>
+                  <th>단백질(g)</th>
+                  <th>지방(g)</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {foodItems.map((item) => (
+                  <tr key={item.id} onDoubleClick={() => addFoodToMeal(item)}>
+                    <td>{item.dietName}</td>
+                    <td>{item.calories}</td>
+                    <td>{item.carbohydrate}</td>
+                    <td>{item.protein}</td>
+                    <td>{item.fats}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
           <div className="modal-actions">
             <button onClick={closeModal}>취소</button>
           </div>
