02aa352da60e139ca35d32ced42c7fd061b904e6
yundoun <dbsehdns12@gmail.com>
2024-01-19 21:32:59 +0900

    Feat : 음식 검색 기능 추가

--- src/components/modals/DietModal.js
+++ src/components/modals/DietModal.js
@@ -5,6 +5,8 @@ import getDietInfo from '../../Api/getDietInfo';
 const DietModal = ({ isModalOpen, closeModal, addFoodToMeal }) => {
 
   const [foodItems, setFoodItems] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
 
   useEffect(() => {
     const fetchFoodItems = async () => {
@@ -15,11 +17,22 @@ const DietModal = ({ isModalOpen, closeModal, addFoodToMeal }) => {
     }
   }, [isModalOpen]);
 
+  const filteredItems = foodItems.filter(item =>
+    item.dietName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     isModalOpen && (
       <div className="modal">
         <div className="modal-content">
           <span className="close" onClick={closeModal}>&times;</span>
+          <input
+            type="text"
+            placeholder="음식 이름으로 검색"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-input"
+          />
           <div className='food-list-container'>
             <table className="food-table">
               <thead>
@@ -32,7 +45,7 @@ const DietModal = ({ isModalOpen, closeModal, addFoodToMeal }) => {
                 </tr>
               </thead>
               <tbody>
-                {foodItems.map((item) => (
+                {filteredItems.map((item) => (
                   <tr key={item.id} onDoubleClick={() => addFoodToMeal(item)}>
                     <td>{item.dietName}</td>
                     <td>{item.calories}</td>
