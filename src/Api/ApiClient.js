2da98d3f163b3f878d11db8737c1a3870ec5793e
yundoun <dbsehdns12@gmail.com>
2024-01-23 00:58:56 +0900

    Feat : url 변경

--- src/Api/ApiClient.js
+++ src/Api/ApiClient.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export const apiClient = axios.create(
   {
-    baseURL: 'http://ec2-52-78-43-76.ap-northeast-2.compute.amazonaws.com:8080'
+    baseURL: 'https://api.healthcare-hwp.com'
   }
 )
